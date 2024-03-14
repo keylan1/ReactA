656501c97381f90bd836727233df3b6d087d0837
Sarah.Keller <sarah29.84@gmail.com>
2024-03-14 19:43:19 +0100

    Basic form component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import App1 from './App1';
+import { useState } from 'react';
 
 function App() {
+  const [name, setName] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setName('');
+    console.log(`Form submitted ${name}`);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <div className="Field">
+            <label htmlFor="name">Name</label>
+            <input
+              id="name"
+              type="text"
+              placeholder="name"
+              name="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <button disabled={!name} type="submit">
+            Submit
+          </button>
+        </fieldset>
+      </form>
+      <App1 />
     </div>
   );
 }
---
+++ src/App1.js
@@ -0,0 +1,5 @@
+import { useState } from 'react';
+
+const App1 = () => {};
+
+export default App1;
