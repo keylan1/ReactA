537cd20a4274efb0910a313aac3aed7668e17baf
Sarah.Keller <sarah29.84@gmail.com>
2024-03-18 17:17:30 +0100

    Added toggle functionality for light/dark mode and ReactSwitch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Form from './components/Form';
 import './App.css';
 import { createContext, useState } from 'react';
+// simple switch package
+import ReactSwitch from 'react-switch';
 
 export const ThemeContext = createContext(null);
 
 // id value (light or dark) sets the light mode in the UI, requires toggling
 function App() {
-  const [theme, setTheme] = useState('dark');
+  const [theme, setTheme] = useState('light');
 
   const toggleTheme = () => {
     setTheme((curr) => (curr === 'light' ? 'dark' : 'light'));
@@ -16,6 +18,10 @@ function App() {
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
       <div className="App" id={theme}>
         <Form />
+        <div className="switch">
+          <label>{theme === 'light' ? 'Light Mode' : 'Dark Mode'}</label>
+          <ReactSwitch onChange={toggleTheme} checked={theme === 'dark'} />
+        </div>
       </div>
     </ThemeContext.Provider>
   );
